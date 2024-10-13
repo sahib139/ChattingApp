1ada52e10875caef3fdfedf973ae01d785595405
sahib singh <ss0990100@gmail.com>
2024-10-13 14:24:16 +0530

    updated backend server to https

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -3,12 +3,21 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const cookieParser = require('cookie-parser');
 const { Server } = require("socket.io");
-const { createServer } = require("http");
+const { createServer } = require("https");  
+const fs = require("fs");
+
 const app = express();
-const server = createServer(app);
-const io = new Server(server,{
+
+const options = {
+    key: fs.readFileSync("./key.pem"),    
+    cert: fs.readFileSync("./cert.pem")   
+};
+
+const server = createServer(options, app);
+
+const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3001",
+        origin: "https://13.60.19.44:3001",
         methods: "*",
         allowedHeaders: "*",
         credentials: true
@@ -20,7 +29,7 @@ const {DB_connect} = require("./config/database-config");
 const {AppRoutes} = require("./routes/index");
 const socketIo = require("./services/webSocket/socket");
 
-app.use(cors({ origin:"http://localhost:3001" , credentials: true }));
+app.use(cors({ origin: "https://13.60.19.44:3001", credentials: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended:true}));
@@ -28,7 +37,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use("/api",AppRoutes);
 
 server.listen(PORT, async () => {
-    console.log(`Server started at port ${PORT}`);
+    console.log(`HTTPS Server started at port ${PORT}`);
     await DB_connect();
     console.log("Database Connected!");
     new socketIo(io);
